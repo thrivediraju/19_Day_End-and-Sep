ef8adbcec74d88f81627c694d7317391ed9d17ab
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-25 21:18:33 +0530

    Add files via upload

---
+++ end_method.py
@@ -0,0 +1,39 @@
+# print('Hi' * 3) # HiHiHi
+# print('Hi ' * 3) # Hi Hi Hi
+# print('Hi\n' * 3,end='') 
+
+# print('hi',end='.')
+# print('hey',end='-')
+
+# print('hi',end='.')
+# print('hey')
+
+# print('hi',end='.')
+# print('hey')
+# print('How') 
+
+# print('hi',end='.')
+# print('hey')
+# print('How are',end=' ')
+# print('you?') 
+
+# print('hi',end='.')
+# print('hey',end=' ')
+# print('How are',end=' ')
+# print('you?') 
+
+''' Combination of end and sep method'''
+# print(11,12,13,14,15,sep='-',end=' ')
+# print(16,17)
+# print(18,19,20)
+
+# print('Python'+' '+'by','Guido Van',end=".",sep='-') # Python by-Guido Van.
+
+# print('hi','bye',sep=' ')
+# print('hello',end='.')
+# print('Py')
+# print('thon')
+
+# print('hi','bye',end=' ')
+# print('hello' 'py',sep='.',end="")
+# print('thon')
